62f814c8b01ae893a55df394aceab6cb78c35bb9
thomascodeur <thomasm@codeur.online>
2018-11-30 14:24:01 +0100

    AjoutCSS + formulaire

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+function preview(input) {
+  if (input.files && input.files[0]) {
+      var freader = new FileReader();
+
+      freader.onload = function (e) {
+          $('#preview').attr('src', e.target.result);
+      }
+      freader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#meme").change(function(){
+  preview(this);
+});
+
