a66c92beec76963cefb1ba84c40f15d870e3e712
thomascodeur <thomasm@codeur.online>
2018-12-03 10:30:00 +0100

    Switch texte/options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+var bouton1 = document.getElementById("texte1");
+var bouton2 = document.getElementById("options1");
+
+var bouton3 = document.getElementById("texte2");
+var bouton4 = document.getElementById("options2");
 
 function preview(input) {
   if (input.files && input.files[0]) {
@@ -13,4 +18,69 @@ function preview(input) {
 $("#meme").change(function(){
   preview(this);
 });
+
+
+bouton1.addEventListener("click",function(e){
+  document.getElementById('texte').style.display = 'flex';
+  document.getElementById('options').style.display = 'none';
+  });
+
+bouton2.addEventListener("click",function(e){
+  document.getElementById('texte').style.display = 'none';
+  document.getElementById('options').style.display = 'flex';
+  });
+
+bouton3.addEventListener("click",function(e){
+  document.getElementById('texte').style.display = 'flex';
+  document.getElementById('options').style.display = 'none';
+  });
+    
+bouton4.addEventListener("click",function(e){
+  document.getElementById('texte').style.display = 'none';
+  document.getElementById('options').style.display = 'flex';
+  });
+
+/*  $(document).ready(function(){
+    var codeTest = '';
+
+    $(document).on('keydown', function(e){
+        if (e.keyCode == 77 && (codeTest == "")) {
+            codeTest += 'm';
+        } else if (e.keyCode == 86 && codeTest == "m") {
+            codeTest += 'v';
+            document.getElementById('Mv').style.display = "flex";
+            setTimeout(function(){
+            document.getElementById('Mv').style.display = "none";
+            },1000);
+        } else {
+            codeTest = "";
+            document.getElementById('Mv').style.display = "none";
+        }
+        console.log(codeTest);
+    });
+
+});
+
+$(document).ready(function(){
+  var codeTest = '';
+
+  $(document).on('keydown', function(e){
+      if (e.keyCode == 66 && (codeTest == "")) {
+        codeTest += 'b';
+    } else if (e.keyCode == 79 && codeTest == "b") {
+        codeTest += 'o';
+    } else if (e.keyCode == 66 && codeTest == "bo") {
+        codeTest += 'b';
+          document.getElementById('Bob').style.display = "flex";
+          setTimeout(function(){
+          document.getElementById('Bob').style.display = "none";
+          },1000);
+      } else {
+          codeTest = "";
+          document.getElementById('Bob').style.display = "none";
+      }
+      console.log(codeTest);
+  });
+
+});*/
 
